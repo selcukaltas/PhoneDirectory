f4ff39c00ecea9c3d9123cdeae56cbb04a3884ae
selcukaltas <altas.selcuk@outlook.com>
2022-02-15 22:47:38 +0300

    Person aggregate root and ContactInformation entity created.

---
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/Person.cs
@@ -0,0 +1,20 @@
+﻿using PhoneDirectory.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryApplicationCore.Domain.PersonAggregate
+{
+    public class Person : IAggregateRoot, IEntity
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Company { get; private set; }
+
+        private readonly List<ContactInformation> _items = new ();
+        public IReadOnlyCollection<ContactInformation> Items => _items.AsReadOnly();
+    }
+}
