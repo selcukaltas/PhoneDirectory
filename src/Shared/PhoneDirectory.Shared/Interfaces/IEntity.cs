0926a009612465f131bb92fa713aeda8b95ff565
selcukaltas <altas.selcuk@outlook.com>
2022-02-15 22:01:08 +0300

    AsyncRepo Created. Common Abstract object created.

---
+++ src/Shared/PhoneDirectory.Shared/Interfaces/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.Shared.Interfaces 
+{ 
+    public interface IEntity
+    {
+        public Guid Id { get; }
+
+    }
+}
---
+++ src/Shared/PhoneDirectory.Shared/Repository/IAsyncRepository.cs
@@ -0,0 +1,20 @@
+﻿using PhoneDirectory.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.Shared.Repository
+{
+    public interface IAsyncRepository<T> where T : IAggregateRoot,IEntity
+    {
+        Task<T> Get(Expression<Func<T, bool>> filter);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+    }
+}
