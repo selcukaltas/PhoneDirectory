973dc92fa18ed0c08ef5f80a3addf79783cebd7c
selcukaltas <altas.selcuk@outlook.com>
2022-02-15 22:22:20 +0300

    EfRepository concrete class and DirectoryDbContext created

---
+++ src/Services/Directory/PhoneDirectory.DirectoryInfrastructure/DirectoryDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using PhoneDirectory.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryInfrastructure
+{
+    public class DirectoryDbContext : DbContext
+    {
+
+        public DirectoryDbContext(DbContextOptions<DirectoryDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var AddedEntities = ChangeTracker.Entries().Where(E => E.Entity is IEntity && E.State == EntityState.Added).ToList();
+
+            AddedEntities.ForEach(E =>
+            {
+                E.Property("CreatedAt").CurrentValue = DateTime.Now;
+                E.Property("UpdatedAt").CurrentValue = DateTime.Now;
+
+            });
+
+            var EditedEntities = ChangeTracker.Entries().Where(E => E.Entity is IEntity && E.State == EntityState.Modified).ToList();
+
+            EditedEntities.ForEach(E =>
+            {
+                E.Property("UpdatedAt").CurrentValue = DateTime.Now;
+            });
+            return base.SaveChangesAsync(cancellationToken);
+        }
+      
+    }
+}
---
+++ src/Services/Directory/PhoneDirectory.DirectoryInfrastructure/EfRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneDirectory.Shared.Interfaces;
+using PhoneDirectory.Shared.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryInfrastructure
+{
+    public class EfRepository<T> : IAsyncRepository<T> where T : class,IEntity, IAggregateRoot
+    {
+        protected readonly DirectoryDbContext _dbContext;
+
+        public EfRepository(DirectoryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<T> Get(Expression<Func<T, bool>> filter)
+        {
+            return await _dbContext.Set<T>().SingleOrDefaultAsync(filter);
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _dbContext.Set<T>().Where(filter).ToListAsync();
+        }
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
+        }
+
+      
+    }
+}
