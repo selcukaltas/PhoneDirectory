166978e7be2d625018fac729e1b70a9d9df28f7c
selcukaltas <altas.selcuk@outlook.com>
2022-02-16 15:05:08 +0300

    Response class and service empty classes created

--- src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/ContactInformation.cs
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/ContactInformation.cs
@@ -11,9 +11,26 @@ namespace PhoneDirectory.DirectoryApplicationCore.Domain.PersonAggregate
     public class ContactInformation : IEntity
     {
         public Guid Id { get; private set; }
-        public InformationType InformationType { get; set; }
-        public string InformationContent { get; set; }
-        public Guid PersonId { get; set; }
+        public InformationType InformationType { get; private set; }
+        public string InformationContent { get; private set; }
+        public Guid PersonId { get; private set; }
+
+        private ContactInformation() { }
+
+        public ContactInformation(Guid informationId, InformationType informationType, string informationContent, Guid personId)
+        {
+
+            if (string.IsNullOrWhiteSpace(personId.ToString())) throw new ArgumentNullException(nameof(personId));
+            if (string.IsNullOrWhiteSpace(informationId.ToString())) throw new ArgumentNullException(nameof(informationId));
+            if (string.IsNullOrWhiteSpace(informationContent)) throw new ArgumentNullException(nameof(informationContent));
+
+
+            Id = personId;
+            InformationType = informationType;
+            InformationContent = informationContent;
+            PersonId = personId;
+        }
+
     }
     public enum InformationType
     {
---
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Services/PersonService.cs
@@ -0,0 +1,14 @@
+﻿using PhoneDirectory.DirectoryApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryApplicationCore.Services
+{
+    public class PersonService : IPersonService
+    {
+
+    }
+}
