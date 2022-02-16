8b9ac3cd78930f561074d339336643d2e1cb109d
selcukaltas <altas.selcuk@outlook.com>
2022-02-17 02:35:39 +0300

    CustomeBaseController created

---
+++ src/Shared/PhoneDirectory.Shared/ControllerBase/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneDirectory.Shared.Result;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneDirectory.Shared.ControllerBase
+{
+    public class CustomBaseController:Controller
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
