91c2ecea68b70750ea944308180ef95be30628f3
selcukaltas <altas.selcuk@outlook.com>
2022-02-16 14:10:25 +0300

    Domain validations completed

--- src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/ContactInformation.cs
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/ContactInformation.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using PhoneDirectory.Shared.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PhoneDirectory.DirectoryApplicationCore.Domain.PersonAggregate
 {
-    public class ContactInformation
+    public class ContactInformation : IEntity
     {
+        public Guid Id { get; private set; }
+        public InformationType InformationType { get; set; }
+        public string InformationContent { get; set; }
+        public Guid PersonId { get; set; }
+    }
+    public enum InformationType
+    {
+        [Description("Telefon Numarası")]
+        PhoneNumber,
+        [Description("E-Mail Adresi")]
+        EmailAddress,
+        [Description("Konum")]
+        Location
     }
 }
--- src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/Person.cs
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Domain/PersonAggregate/Person.cs
@@ -14,7 +14,27 @@ namespace PhoneDirectory.DirectoryApplicationCore.Domain.PersonAggregate
         public string Surname { get; private set; }
         public string Company { get; private set; }
 
-        private readonly List<ContactInformation> _items = new ();
-        public IReadOnlyCollection<ContactInformation> Items => _items.AsReadOnly();
+        private readonly List<ContactInformation> _contactInformations = new ();
+        public IReadOnlyCollection<ContactInformation> ContactInformations => _contactInformations.AsReadOnly();
+
+
+        private Person() { } //Ef Required
+
+        public Person(Guid personId, string name, string surname,string company, List<ContactInformation> contactInformations)
+        {
+            if (string.IsNullOrWhiteSpace(personId.ToString())) throw new ArgumentNullException(nameof(personId));
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(company)) throw new ArgumentNullException(nameof(company));
+            if (string.IsNullOrWhiteSpace(surname)) throw new ArgumentNullException(nameof(surname));
+
+
+            Id = personId;
+
+            Name = name;
+
+            Surname = surname;
+            Company = company;
+            _contactInformations = contactInformations;
+        }
     }
 }
