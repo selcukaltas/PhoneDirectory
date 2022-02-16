078441b089ed9bcdbad4c4ffd467706d0c2e9777
selcukaltas <altas.selcuk@outlook.com>
2022-02-16 20:50:55 +0300

    Automapper nuget package installed.

---
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Mapper/PersonProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using PhoneDirectory.DirectoryApplicationCore.Domain.PersonAggregate;
+using PhoneDirectory.DirectoryApplicationCore.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryApplicationCore.Mapper
+{
+    public class PersonProfile : Profile
+    {
+        public PersonProfile()
+        {
+            CreateMap<PersonDto, Person>().ReverseMap();
+        }
+    }
+}
--- src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Services/PersonService.cs
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Services/PersonService.cs
@@ -1,4 +1,8 @@
-﻿using PhoneDirectory.DirectoryApplicationCore.Interfaces;
+﻿using PhoneDirectory.DirectoryApplicationCore.Domain.PersonAggregate;
+using PhoneDirectory.DirectoryApplicationCore.DTOs;
+using PhoneDirectory.DirectoryApplicationCore.Interfaces;
+using PhoneDirectory.Shared.Repository;
+using PhoneDirectory.Shared.Result;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +13,35 @@ namespace PhoneDirectory.DirectoryApplicationCore.Services
 {
     public class PersonService : IPersonService
     {
+        private readonly IAsyncRepository<Person> _personRepository;
+        private readonly IAsyncRepository<Person> _personRepository;
+        public PersonService(IAsyncRepository<Person> personRepository)
+        {
+            _personRepository = personRepository;
+        }
+        public Task<Response<PersonDto>> CreatePerson(PersonDto personDto)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<Response<NoContent>> DeletePerson(Guid personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<List<PersonDto>>> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<PersonDto>> GetPerson(Guid personId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<PersonDetailDto>> GetPersonDetail(Guid personId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
