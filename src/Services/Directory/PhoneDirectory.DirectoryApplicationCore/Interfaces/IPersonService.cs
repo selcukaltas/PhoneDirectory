1b41a3b4c5b1d0827bceeaf5b674252ee5bb8716
selcukaltas <altas.selcuk@outlook.com>
2022-02-16 18:01:32 +0300

    Dtos created

--- src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Interfaces/IPersonService.cs
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Interfaces/IPersonService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PhoneDirectory.DirectoryApplicationCore.DTOs;
+using PhoneDirectory.Shared.Result;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,10 @@ namespace PhoneDirectory.DirectoryApplicationCore.Interfaces
 {
     public interface IPersonService
     {
-
+        Task<Response<PersonDto>> CreatePerson(PersonDto personDto);
+        Task<Response<NoContent>> DeletePerson(Guid personId);
+        Task<Response<List<PersonDto>>> GetAllPersons();
+        Task<Response<PersonDto>> GetPerson(Guid personId);
+        Task<Response<PersonDetailDto>> GetPersonDetail(Guid personId);
     }
 }
