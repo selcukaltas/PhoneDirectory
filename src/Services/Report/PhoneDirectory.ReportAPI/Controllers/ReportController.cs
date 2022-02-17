726f0f062a5da46a01b90148c45b89d65c28dbee
selcukaltas <altas.selcuk@outlook.com>
2022-02-17 16:11:29 +0300

    ReportController added

---
+++ src/Services/Report/PhoneDirectory.ReportAPI/Controllers/ReportController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhoneDirectory.ReportApplicationCore.Services;
+using PhoneDirectory.Shared.ControllerBase;
+
+namespace PhoneDirectory.ReportAPI.Controllers
+{
+    public class ReportController : CustomBaseController
+    {
+        private readonly IReportService _reportService;
+
+        public ReportController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReportRequest()
+        {
+            var result = await _reportService.CreateReport();
+
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllReports()
+        {
+            var result = await _reportService.GetAllReports();
+            return CreateActionResultInstance(result);
+        }
+
+        [HttpGet("{reportId}")]
+     
+        public async Task<IActionResult> GetReportDetail(Guid reportId)
+        {
+            var result = await _reportService.GetReportDetail(reportId);
+
+            return CreateActionResultInstance(result);
+        }
+     
+    }
+}
