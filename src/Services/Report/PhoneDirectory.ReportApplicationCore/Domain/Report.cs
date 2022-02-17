f2f70029a57bb30ac155dddc7918a87f0b38bed9
selcukaltas <altas.selcuk@outlook.com>
2022-02-17 04:26:20 +0300

    Report Domains created

---
+++ src/Services/Report/PhoneDirectory.ReportApplicationCore/Domain/Report.cs
@@ -0,0 +1,39 @@
+﻿using PhoneDirectory.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.ReportApplicationCore.Domain
+{
+    public class Report : IAggregateRoot, IEntity
+    {
+        public DateTime RequestDate { get; private set; }
+        public ReportStatus ReportStatus { get; private set; }
+
+        private readonly List<ReportDetail> _reportDetails = new();
+        public IReadOnlyCollection<ReportDetail> ContactInformations => _reportDetails.AsReadOnly();
+        public Guid Id { get; private set; }
+
+        private Report()
+        {
+
+        }
+        public Report(Guid id, DateTime requestDate, ReportStatus reportStatus)
+        {
+            if (string.IsNullOrWhiteSpace(id.ToString())) throw new ArgumentNullException(nameof(id));
+            Id = id;
+            RequestDate = requestDate;
+            ReportStatus = reportStatus;
+        }
+    }
+    public enum ReportStatus
+    {
+        [Description("Hazırlanıyor")]
+        Preparing,
+        [Description("Tamamlandı")]
+        Completed
+    }
+}
---
+++ src/Services/Report/PhoneDirectory.ReportApplicationCore/Domain/ReportDetail.cs
@@ -0,0 +1,33 @@
+﻿using PhoneDirectory.Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.ReportApplicationCore.Domain
+{
+    public class ReportDetail : IEntity
+    {
+        public string Location { get; private set; }
+        public int PersonCount { get; private set; }
+        public int NumberCount { get; private set; }
+        public Guid ReportId { get; private set; }
+        public Guid Id { get; private set; }
+
+        private ReportDetail() { }
+
+        public ReportDetail(Guid id, Guid reportId, int personCount, int numberCount, string location)
+        {
+            if (string.IsNullOrWhiteSpace(id.ToString())) throw new ArgumentNullException(nameof(id));
+            if (string.IsNullOrWhiteSpace(reportId.ToString())) throw new ArgumentNullException(nameof(reportId));
+            if (string.IsNullOrWhiteSpace(location)) throw new ArgumentNullException(nameof(location));
+            Id = id;
+            ReportId = reportId;
+            PersonCount = personCount;
+            NumberCount = numberCount;
+            Location = location;
+
+        }
+    }
+}
