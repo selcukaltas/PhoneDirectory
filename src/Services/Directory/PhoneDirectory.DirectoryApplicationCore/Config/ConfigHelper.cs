dc6ce41b2377c0fc859adece976ec98307bcadcd
selcukaltas <altas.selcuk@outlook.com>
2022-02-17 16:39:33 +0300

    CommunicationService and ExceptionMiddleware created.

---
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Config/ConfigHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryApplicationCore.Config
+{
+    public class ConfigHelper
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string RabbitMqCon => _configuration["RabbitMq"] ?? "";
+
+    }
+}
---
+++ src/Services/Directory/PhoneDirectory.DirectoryApplicationCore/Services/CommunicationService.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using PhoneDirectory.DirectoryApplicationCore.Interfaces;
+using PhoneDirectory.Shared.Result;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirectory.DirectoryApplicationCore.Services
+{
+    public class CommunicationService : ICommunicationService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public CommunicationService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<Response<NoContent>> Publish()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, $"http://localhost:5001/api/ReportRequest/");
+            var response = await client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+                return Response<NoContent>.Fail("Something went wrong",500);
+
+            var responseStream = await response.Content.ReadAsStringAsync();
+
+            
+             var reportId = JsonConvert.DeserializeObject<Guid>(responseStream)
+
+            var conn = _phoneBookSettings.RabbitMqCon;
+
+            var createDocumentQueue = "create_document_queue";
+            var documentCreateExchange = "document_create_exchange";
+
+            ConnectionFactory connectionFactory = new()
+            {
+                Uri = new Uri(conn)
+            };
+
+            var connection = connectionFactory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare(documentCreateExchange, "direct");
+
+            channel.QueueDeclare(createDocumentQueue, false, false, false);
+            channel.QueueBind(createDocumentQueue, documentCreateExchange, createDocumentQueue);
+
+            channel.BasicPublish(documentCreateExchange, createDocumentQueue, null, Encoding.UTF8.GetBytes(reportId.ToString()));
+        }
+    }
+}
